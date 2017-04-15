d8b14b249e20d755f96838137e0716ef84006389
Sean Frisbey <seanfrisbey@gmail.com>
2017-04-14 19:53:30 -0700

    Initial commit
    Imitial commit.

---
+++ P1/Program.cs
@@ -0,0 +1,22 @@
+﻿/***********************************************************
+ * Write a program that reads a text file and displays the
+ * number of words.
+ **********************************************************/
+
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputTextFile = File.ReadAllText(@"..\..\..\testFile.txt");
+            var words = inputTextFile.Split(' ');
+            Console.WriteLine("The number of words in the text file is {0}", words.Count());
+        }
+    }
+}
---
+++ P2/Program.cs
@@ -0,0 +1,27 @@
+﻿/***********************************************************
+ * Write a program that reads a text file and displays the
+ * longest word in the file.
+ **********************************************************/
+
+using System;
+using System.IO;
+
+namespace P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputTextFile = File.ReadAllText(@"..\..\..\testFile.txt");
+            var words = inputTextFile.Split(' ');
+
+            int longestWord = 0;
+            for (int i = 1; i < words.Length; i++)
+            {
+                longestWord = (words[i].Length > words[i - 1].Length) ? i : i-1;
+            }
+
+            Console.WriteLine("The longest word in the text file is \"{0}\"", words[longestWord]);
+        }
+    }
+}
